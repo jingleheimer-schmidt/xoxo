b451d7d117cc41601bfc427f58a26608acd87a6e
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2026-06-11 23:24:39 -0700

    Add custom inputs for hug and kiss actions
    Register two custom-input prototypes in data.lua: 'xoxo-hug' (keys 'o'/'O') and 'xoxo-kiss' (keys 'x'/'X'), both set to trigger a lua action. These are added to the data table via data:extend.

--- data.lua
+++ data.lua
@@ -1,1 +1,22 @@
 
+---@type data.CustomInputPrototype
+local hugs = {
+    type = "custom-input",
+    name = "xoxo-hug",
+    key_sequence = "o",
+    alternative_key_sequence = "O",
+    action = "lua",
+}
+
+local kisses = {
+    type = "custom-input",
+    name = "xoxo-kiss",
+    key_sequence = "x",
+    alternative_key_sequence = "X",
+    action = "lua",
+}
+
+data:extend {
+    hugs,
+    kisses,
+}
